6b317803d18a5d2c1da94d6441b52bf9bb37408f
huangpy <726859314@qq.com>
2020-11-04 16:01:28 +0800

    添加到cocoapods和从cocoapods下载

---
+++ HFBaseUtilDemo/Pods/Target Support Files/HFBaseUtils/HFBaseUtils-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HFBaseUtils : NSObject
+@end
+@implementation PodsDummy_HFBaseUtils
+@end
---
+++ HFBaseUtilDemo/Pods/Target Support Files/Pods-HFBaseUtilDemo/Pods-HFBaseUtilDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HFBaseUtilDemo : NSObject
+@end
+@implementation PodsDummy_Pods_HFBaseUtilDemo
+@end
